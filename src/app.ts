7584f50553e9ae8374b6413f135388ce6743a6ac
Ujjwal Karn <43888360+UJKEM@users.noreply.github.com>
2023-08-21 21:14:21 +0530

    Added cors and helmet

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import express, { Application } from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
 import routes from './routes';
 import { errorHandler } from './middlewares/errorHandler';
 
 const app: Application = express();
+
+app.use(cors());
+
+app.use(helmet());
 
 app.use(bodyParser.urlencoded({extended: true}))
 
